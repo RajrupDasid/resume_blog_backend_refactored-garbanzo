50efeeedd747c2e716fbc324caae96119f6164c4
RajrupDasid <rajrupofficialid@protonmail.com>
2023-10-04 17:02:12 +0530

    Added additional field
    Additonal fields added
    
    Signed-off-by: RajrupDasid <rajrupofficialid@protonmail.com>

--- controller/admin.py
+++ controller/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
-
+from .models import Blog
 # Register your models here.
+
+
+@admin.register(Blog)
+class BlogPostAdmin(admin.ModelAdmin):
+    exclude = ['slug',]
+    list_display = ('_id', 'title', 'created', 'updated',)
+    list_display_links = ('_id', 'title')
+    list_per_page = 30
