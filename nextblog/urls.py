bfd032194680c9b6b08c28bc4ae22cd17415483e
RajrupDasid <rajrupofficialid@protonmail.com>
2023-10-04 17:20:25 +0530

    Controller urls added
    Signed-off-by: RajrupDasid <rajrupofficialid@protonmail.com>

--- nextblog/urls.py
+++ nextblog/urls.py
@@ -5,6 +5,7 @@ from django.conf.urls.static import static
 from decouple import config
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/', include('controller.urls')),
     path('ckeditor/', include('ckeditor_uploader.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
