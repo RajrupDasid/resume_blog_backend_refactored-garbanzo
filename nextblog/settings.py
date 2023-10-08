5e6cd6e571d8dca18da000b6b57ebeb5db54e2f9
RajrupDasid <rajrupofficialid@protonmail.com>
2023-10-08 23:47:59 +0530

    AWS related settings changed for production
    Signed-off-by: RajrupDasid <rajrupofficialid@protonmail.com>

--- nextblog/settings.py
+++ nextblog/settings.py
@@ -236,6 +236,7 @@ CKEDITOR_UPLOAD_PATH = "uploads/"
 CKEDITOR_IMAGE_BACKEND = "pillow"
 CKEDITOR_CONFIGS = {
     'default': {
+        'removePlugins': 'stylesheetparser',
         'toolbar': 'full',
         'height': 900,
         'width': 900,
@@ -304,28 +305,28 @@ PASSWORD_RESET_TIME_OUT = 900  # in seconds
 
 # Logging setup
 # HTTPS settings
-SESSION_COOKIE_SECURE = True
-CSRF_COOKIE_SECURE = True
-SECURE_SSL_REDIRECT = True
-##
-# HSTS settings
-SECURE_HSTS_SECONDS = 31536000
-SECURE_HSTS_PRELOAD = True
-SECURE_HSTS_INCLUDE_SUBDOMAINS = True
-
+if PRODUCTION:
+    SESSION_COOKIE_SECURE = True
+    CSRF_COOKIE_SECURE = True
+    SECURE_SSL_REDIRECT = True
+    ##
+    # HSTS settings
+    SECURE_HSTS_SECONDS = 31536000
+    SECURE_HSTS_PRELOAD = True
+    SECURE_HSTS_INCLUDE_SUBDOMAINS = True
 if PRODUCTION:
     AWS_ACCESS_KEY_ID = config('AWS_ACCESS_KEY_ID')
     AWS_SECRET_ACCESS_KEY = config('AWS_SECRET_ACCESS_KEY')
     AWS_STORAGE_BUCKET_NAME = config('AWS_STORAGE_BUCKET_NAME')
-    AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
     AWS_S3_SIGNATURE_NAME = 's3v4',
     AWS_S3_REGION_NAME = config('AWS_S3_REGION_NAME')
     AWS_S3_FILE_OVERWRITE = False
     AWS_DEFAULT_ACL = None
     AWS_S3_VERITY = True
+    AWS_QUERYSTRING_AUTH = False
     DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
     STATICFILES_STORAGE = "storages.backends.s3.S3Storage"
     S_MODE = "static"
     M_MODE = "media"
-    STATIC_URL = f'http://{AWS_S3_CUSTOM_DOMAIN}/{S_MODE}/'
-    MEDIA_URL = f'http://{AWS_S3_CUSTOM_DOMAIN}/{M_MODE}/'
+    STATIC_URL = f'https://{AWS_STORAGE_BUCKET_NAME}/'
+    MEDIA_URL = f'https://{AWS_STORAGE_BUCKET_NAME}/'
