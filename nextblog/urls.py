7d40bfa7fe12e50b6913cf5033fdc4e3029f8919
RajrupDasid <rajrupofficialid@protonmail.com>
2023-10-08 13:07:50 +0530

    Debug media render group added
    Signed-off-by: RajrupDasid <rajrupofficialid@protonmail.com>

--- nextblog/urls.py
+++ nextblog/urls.py
@@ -7,5 +7,8 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('controller.urls')),
     path('ckeditor/', include('ckeditor_uploader.urls')),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+]
+if settings.DEBUG:
+    + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.STATIC_URL,
+                          document_root=settings.STATIC_ROOT)
