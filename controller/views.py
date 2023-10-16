113ca28c88d3275080a36fbd111dcf90858baee8
RajrupDasid <rajrupofficialid@protonmail.com>
2023-10-16 20:13:43 +0530

    Contact api view added
    Signed-off-by: RajrupDasid <rajrupofficialid@protonmail.com>

--- controller/views.py
+++ controller/views.py
@@ -2,13 +2,14 @@ from django.shortcuts import render
 from rest_framework.views import APIView
 from rest_framework import permissions
 from .models import Blog
-from .serializers import BlogViewSerializer, BlogDetailSerializer
+from .serializers import BlogViewSerializer, BlogDetailSerializer, ContactSerializer
 from rest_framework.response import Response
 from django.shortcuts import get_object_or_404
 from rest_framework_api_key.permissions import HasAPIKey
 from django.utils.decorators import method_decorator
 from django.views.decorators.cache import cache_page
 from django.core.cache.backends.base import DEFAULT_TIMEOUT
+from rest_framework import status
 from django.conf import settings
 
 CACHE_TTL = getattr(settings, 'CACHE_TTL', DEFAULT_TIMEOUT)
@@ -39,3 +40,16 @@ class BlogDetailView(APIView):
 
     def post(self, request):
         return Response({'error': 'Something went wrong. Please try again.'}, status=400)
+
+
+class Contact(APIView):
+    permission_classes = [HasAPIKey]
+
+    @method_decorator(cache_page(CACHE_TTL))
+    def post(self, request, *args, **kwargs):
+        serializer = ContactSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response({'msg': 'Contact hasbeen save into database successfully'}, status=status.HTTP_201_CREATED)
+        else:
+            return Response({'error': 'Error you need to fillup the form'}, status=status.HTTP_400_BAD_REQUEST)
