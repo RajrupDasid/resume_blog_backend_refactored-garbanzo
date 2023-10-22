39a61ebe69e1dd8e0f63f0140ba84df05a9f79d6
Rajrup Das <rajrupofficialid@protonmail.com>
2023-10-22 08:34:41 +0000

    Added category based views (#7)
    Signed-off-by: RajrupDasid <rajrupofficialid@protonmail.com>

--- controller/urls.py
+++ controller/urls.py
@@ -3,6 +3,7 @@ from .views import IndexAPIView, BlogDetailView, Contact
 
 urlpatterns = [
     path('blogs/', IndexAPIView.as_view(), name="index"),
-    path('blogs/<str:slug>', BlogDetailView.as_view(), name="detailview"),
+    path('blogs/<str:category>/<str:slug>',
+         BlogDetailView.as_view(), name="detailview"),
     path('contacts/', Contact.as_view(), name="contactview")
 ]
