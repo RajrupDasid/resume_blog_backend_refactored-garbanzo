cfa0f9d8f80e5f1097d0f21b4ddada0e34c5da3c
RajrupDasid <rajrupofficialid@protonmail.com>
2023-11-04 16:28:00 +0530

    Upgraded
    Signed-off-by: RajrupDasid <rajrupofficialid@protonmail.com>

--- upgradescript.sh
+++ upgradescript.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 echo "Processing update please wait"
 # Update Django and other dependencies
-pip install -U -r requirements.txt
+while read -r package; do
+    package_name=$(echo "$package" | awk -F'==' '{print $1}')
+    pip install -U "$package_name"
+done < requirements.txt
 echo "Dependencies updated successfully."
