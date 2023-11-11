1326cce1bb213b031330b9c4b6607f81b0ff2653
RajrupDasid <rajrupofficialid@protonmail.com>
2023-11-11 17:15:59 +0530

    Analytics metrics added
    Signed-off-by: RajrupDasid <rajrupofficialid@protonmail.com>

--- analytics/admin.py
+++ analytics/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
-
+from .models import Analytics
 # Register your models here.
+
+
+@admin.register(Analytics)
+class ContactAdmin(admin.ModelAdmin):
+    list_display = ('id', "post_id", "post_clicks", "created")
+    list_display_links = ("id", "post_id")
+    list_per_page = 30
